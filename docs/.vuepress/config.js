6f8a16d0302269e46f4050f888a3d85c6c5f53ee
Ybalabala0424 <17myzhang6@stu.edu.cn>
2019-06-04 22:28:13 +0800

    vuepressStudy

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    title: '瑶九九BBlog介绍',
+    base: '/VuePress/',
+    themeConfig: {
+        sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
+        lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
+        sidebar: {
+            '/Intro/':
+                [{
+                    title: '介绍',
+                    children: [
+                        ['/Intro/BlogIntro.md', '博客介绍']
+                    ]
+                }
+                ],
+            '/guide/':
+                [{
+                    title: '介绍',
+                    children: [
+                        ['/guide/use.md', '使用说明']
+                    ]
+                }
+                ]
+        },
+            nav: [
+                {text: '首页', link: '/'},
+                {text: '博客介绍', link: '/Intro/BlogIntro.md'},
+                {text: '使用说明', link: '/guide/use.md'},
+                {text: '博客', link: 'https://google.com'},
+            ]
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,21 @@
+---
+
+home: true
+heroImage: /logo.jpg
+actionText: 入门到删库跑路→
+actionLink: /Intro/BlogIntro.md
+features:
+
+- title: 博客介绍
+  details: 介绍博客内容及开发心路历程。
+
+- title: 使用说明
+
+  details: 说明编译软件和环境，讲解使用方法。
+
+- title: 待开发
+  details: 还在持续努力中。
+
+footer: 诚信加盟​  |秃如其来 ​​|只差一个程序员了
+
+---
