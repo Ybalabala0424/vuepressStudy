beff58fd88d94658615f3952795d4dc54ffded02
Ybalabala0424 <17myzhang6@stu.edu.cn>
2019-06-04 22:30:40 +0800

    修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     title: '瑶九九BBlog介绍',
-    base: '/VuePress/',
+    base: '/vuepress/',
     themeConfig: {
         sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
         lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
